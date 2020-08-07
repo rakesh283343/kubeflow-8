053b34351ad8c79c19a2d839d1bc6de3817625a7
wondermelon4ever <wondermelonforever@gmail.com>
2020-08-07 04:43:49 +0000

    updated

--- preprocess/titanic_preprocess.py
+++ preprocess/titanic_preprocess.py
@@ -1,12 +1,36 @@
-{
- "cells": [],
- "metadata": {
-  "environment": {
-   "name": "common-cpu.m54",
-   "type": "gcloud",
-   "uri": "gcr.io/deeplearning-platform-release/base-cpu:m54"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+#titanic_preprocess.py
+#Copyright 2020 Google LLC. 
+#This software is provided as-is, without warranty or representation for any use or purpose. 
+#Your use of it is subject to your agreements with Google.
+#Author: whjang@google.comimport sys
+import pandas as pd
+import argparsedef preprocessing(raw_csv_gcs_uri, preproc_csv_gcs_uri): 
+ print(“Preprocessing Titanic Data”)
+ train_raw = pd.read_csv(raw_csv_gcs_uri)
+ 
+ features = [“Pclass”, “Sex”, “SibSp”]
+ preprocessing_data = pd.get_dummies(train_raw[features])
+ preprocessing_data[“Survived”] = train_raw[“Survived”] print(preprocessing_data.head())
+ preprocessing_data.to_csv(preproc_csv_gcs_uri, index=False)if __name__ == “__main__”:
+ 
+ arg_parser = argparse.ArgumentParser()
+ 
+ arg_parser.add_argument(
+ “--raw_csv_gcs_uri”,
+ type=str,
+ required=True,
+ help=”Google Cloud Storage URI for reading raw train data (ex: gs://{bucketName}/{pathToRaw}/{rawTrainFile}.csv)”
+ )
+ 
+ arg_parser.add_argument(
+ “--preproc_csv_gcs_uri”,
+ type=str,
+ required=True,
+ help=”Google Cloud Storage URI for writing pre-processed train data (ex: gs://{bucketName}/{pathToRaw}/{rawTrainFile}.csv)”
+ )
+ 
+ # AI Platform Job submit throws unknown arguments
+ args, unknown = arg_parser.parse_known_args()
+ 
+ preprocessing(args.raw_csv_gcs_uri, args.preproc_csv_gcs_uri)
+    
